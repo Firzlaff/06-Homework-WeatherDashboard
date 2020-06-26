c46dfef81d9bc69cb9b9ad5c14d942a3a7f59943
Firzlaff <tysenfirzlaff@gmail.com>
2020-06-26 12:03:19 -0600

    got button added and working added in both api calls and got data pulling for current cityupdated html added further sudo

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
-//WHEN I search for a city
+$(document).ready(function () {
 
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
+// pull search history from local storage and display into the 
+  // const todaysDate = moment().format('LL');
+  // $("#date").append(todaysDate);
 
-//WHEN I view current weather conditions for that city
+
+// on click function for button to search city
+  $('#searchBtn').on("click", function (event) {
+    event.preventDefault();
+    console.log("button CLicked");
+
+    const searchValue = $("#searchValue").val();
+    
+    searchCity(searchValue);
+    
+  });
+  
+  //function to take the search value and pull the data from openweather api for current city.
+  function searchCity(searchValue) {
+    $.ajax({
+      type: "POST",
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=f9ad587c342647080ebc824ff8e4442d&units=imperial`,
+      dataType: "json",
+    }).then(function (response) {
+      console.log(response);
+      $("#cityName").text(response.name);
+      $("#temp").text(response.main.temp);
+      $("#speed").text(response.wind.speed);
+      $("#humidity").text(response.main.humidity);
+     
+      
+      //call to get the 5 days in the onecall api to openweather 
+        $.ajax({
+          type: "POST",
+          url: `https://api.openweathermap.org/data/2.5/onecall?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=f9ad587c342647080ebc824ff8e4442d&units=imperial`,
+          dataType: "json",
+        }).then(function (response) {
+          console.log(response);
+          $("#uvDex").text(response.current.uvi);
+
+          //add the city name to the search history array 
+          // store the array  user stringify to save it to the local storage 
+
+
+        })
+      
+
+
+    })}
+
+
+    
+    //WHEN I search for a city
+    //THEN I am presented with current and future conditions for that city and that city is added to the search history
+    //WHEN I view current weather conditions for that city
 
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index 
 
@@ -21,3 +72,7 @@
 //WHEN I open the weather dashboard
 
 //THEN I am presented with the last searched city forecast
+
+// AJAX call for open weather 
+
+})
